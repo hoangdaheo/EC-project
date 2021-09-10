c3a297b700e250b53c46491df1c3562fcb1c3722
Nguyễn Nguyên Hoàng <hoangdaheodaheo@gmail.com>
2021-09-10 09:01:10 +0700

    adding header comp and link to home,shoppage and contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
